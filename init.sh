bee64442982ba3ac63b001714c004f3c3156ef94
Kushal Arora <karora@s1.(none)>
2013-10-12 09:27:31 -0700

    some modification to init.sh file

--- init.sh
+++ init.sh
@@ -2,11 +2,14 @@
 
 module load novaclient
 
-if  test ! -s ${HOME}/.futuregrid/novarc
+NOVARC=${HOME}/.futuregrid/novarc
+echo $NOVARC
+if  test ! -s $NOVARC
 then
      module load cloudmesh
      cm-manage config sierra-openstack-grizzly
 fi
+. ${NOVARC}
 
 python ${HOME}/OpenStackVaccine/instance.py
 

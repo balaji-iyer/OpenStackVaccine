9cc1ed2a542138e53b089123efa5bbc4f1f94da3
kushal <kushal@kushal-Vostro-3550.(none)>
2013-11-19 13:51:46 -0500

    renamed get_client as wrapper

--- controller/registrar.py
+++ controller/registrar.py
@@ -28,13 +28,15 @@ class Registrar:
 
 
         try:
-            client = getattr(__import__(self.client_dir.replace("/", ".") + "." + 'get_client'), 'get_client')
+            # SO/10675054 Notice . in front of wrapper.
+            # This is to specify relative path of wrapper w.r.t package
+            wrapper = importlib.import_module(".wrapper", self.client_dir.replace("/", "."))
             #client_mod = __import__(self.client_dir.replace("/", "."))
         except ImportError:
             print "Please check dir path. Ensure it has __init__.py file"
             sys.exit(-1)
 
-        self.client = client.get_client(conf["menaces"], conf["processes"], auth_info)
+        self.client = wrapper.get_client(conf["menaces"], conf["processes"], auth_info)
 
 
     def get_client(self):

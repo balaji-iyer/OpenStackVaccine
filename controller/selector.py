1359cb1cb85d04786231049887d59f1b692d4759
kushal <kushal@kushal-Vostro-3550.(none)>
2013-11-19 12:05:59 -0500

    selector and selector_test added

--- controller/selector.py
+++ controller/selector.py
@@ -1,20 +1,51 @@
-"""
-    Selects which type of menace to make.
-"""
+from menace.menace import Menace
+import random
+import sys
 
 class Selector:
-    def __init__(self):
-        pass
+    """ Selector class.
+        Decides upon which type of menace to make.
+        Which instances to make this menace on.
+        If process or volume related, returns a random
+        process or volume too respectively.
+    """
+    def __init__(self, client):
+        self.client = client
 
-    def select_menace(self, client):
-        pass
+    def select_menace(self):
+        menaces = self.client.menaces
+        if menaces == None or len(menaces) == 0:
+            print "Menace list missing. Check clients.json"
+            sys.exit(-1)
 
-    def select_process(self, client):
-        pass
+        return self._select_random(menaces)
 
-    def select_instance(self, client, process):
-        pass
+
+    def select_instance(self):
+        instances = self.client.list_instances()
+
+        if instances == None or len(instances) == 0:
+            print "No instance up right now. Nothing to do"
+            return None
+        return self._select_random(instances)
+
+    def select_process(self, menace):
+
+        if menace != Menace.KILL_PROCESS:
+            print "Wrong menace type: %s" % menace
+            sys.exit(-1)
+
+        processes = self.client.processes
+        if processes == None or len(processes) == 0:
+            print "Process list missing. Check clients.json"
+            sys.exit(-1)
+
+        return self._select_random(processes)
+
 
     def select_volume(self, client, instance):
         pass
 
+    def _select_random(self, list):
+        index = int(random.uniform(0, len(list)))
+        return list[index]
---
+++ test/selector_test.py
@@ -0,0 +1,41 @@
+from controller.selector import Selector
+from controller.registrar import Registrar
+from menace.menace import Menace
+import json
+
+
+class TestSelector:
+    def __init__(self, client):
+        self.client = client
+        self.selector = Selector(client)
+
+    def test_select_menace(self):
+        menace = self.selector.select_menace()
+        assert menace in self.client.menaces
+        print menace
+
+    def test_select_instace(self):
+        instance = self.selector.select_instance()
+        assert instance in self.client.list_instances()
+        print instance
+
+    def test_select_process(self):
+        process = self.selector.select_process(Menace.KILL_PROCESS)
+        assert process in self.client.processes
+        print process
+
+
+if __name__ == "__main__":
+    conf_file = open("../config/clients.json")
+    conf_json = json.load(conf_file)
+
+    client = conf_json[0]
+
+    auth_info = json.load(open("../config/auth.json"))[client["name"]]
+    registrar = Registrar(client, auth_info)
+
+    test = TestSelector(registrar.client)
+    test.test_select_menace()
+    test.test_select_instace()
+    test.test_select_process()
+
